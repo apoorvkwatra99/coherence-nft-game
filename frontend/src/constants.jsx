fbddea99857fb423c51358709920a1fce1b367c2
Apoorv Kwatra <71152452+apoorvkwatra99@users.noreply.github.com>
2024-12-09 23:07:26 +0100

    Update frontend

---
+++ frontend/src/constants.jsx
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0xE5E659818bdfa8bE9903Ae8624Dc12918450d186';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
